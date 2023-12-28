ab81a56ac729697b16db00cfef3ed3f07e8122ea
Au Grimace <augrimace@gmail.com>
2023-12-27 19:34:49 -0800

    initial comit

--- lib/portfolio-cdk-stack.ts
+++ lib/portfolio-cdk-stack.ts
@@ -1,16 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as pipelines from 'aws-cdk-lib/pipelines';
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codepipelineActions from '@aws-cdk/aws-codepipeline-actions';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as codebuild from '@aws-cdk/aws-codebuild';
 
 export class PortfolioCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Define an S3 bucket to store your application artifacts (e.g., build outputs).
+    const artifactBucket = new s3.Bucket(this, 'ArtifactBucket');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'PortfolioCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Define a CodeBuild project to build your React application.
+    const buildProject = new codebuild.PipelineProject(this, 'BuildProject', {
+      // Specify build settings, environment, and other configurations here.
+    });
+
+    // Define your CodePipeline.
+    const pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
+      artifactBucket,
+    });
+
+    // Define pipeline stages and actions here.
+    // For example, add source and build stages using codepipelineActions.
+
+    // ...
   }
 }
